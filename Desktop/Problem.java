d8ef217aa4f7dbcbf6aa14b04386e9009464df91
rafcamaroun <rafcamaroun04@gmail.com>
2023-01-27 20:42:58 +0200

    Problem ex

---
+++ Desktop/Problem.java
@@ -0,0 +1,9 @@
+public class Problem {
+	public static void main (Static[] args); {
+		System.out.print("Hi, can we check this?");
+		System.out.println(" It has a lot of problems');
+		System.out.println("Yeah, I know !!!");
+		// This is a comment;
+
+	}
+}
