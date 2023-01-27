e5ae5ab87ebc2b89abcb34d66216044d00b8c51f
rafcamaroun <rafcamaroun04@gmail.com>
2023-01-27 20:50:03 +0200

    initial info provided

---
+++ Desktop/Profile.java
@@ -0,0 +1,8 @@
+public class Profile {
+	public static void main(String[] args)
+	{
+	System.out.println("Rafca Maroun")
+	System.out.println("Electrical Engineering")
+	System.out.println("2023")
+	}
+}
