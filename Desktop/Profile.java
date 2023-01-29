a29ff9ea70746497a350607af3599e352bb5392d
rafcamaroun <rafcamaroun04@gmail.com>
2023-01-29 12:46:13 +0200

    my hobby is added

--- Desktop/Profile.java
+++ Desktop/Profile.java
@@ -4,5 +4,6 @@ public class Profile {
 	System.out.println("Rafca Maroun")
 	System.out.println("Electrical Engineering")
 	System.out.println("2023")
+	System.out.println("Card Tricks and Scams")
 	}
 }
